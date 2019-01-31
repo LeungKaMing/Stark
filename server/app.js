b3760536e532fe74dfb84dec4988ab81cbdb2e8c
梁珈铭 <liangjiaming1@ppmoney.com>
2019-01-31 17:54:43 +0800

    feat: ssr改造

--- server/app.js
+++ server/app.js
@@ -1,9 +1,5 @@
 const http = require('http')
-// const routes = require('./routes/index')
-
-// fe
-const App = require('./static/entry/serverEntry')	// react服务端代码用es6需要在这里处理
-console.log(App.markup(1), '<<<<<<')
+const routes = require('./routes/index')
 
 // 0. 模版引擎写死脚本(第三方cdn资源)
 // 1. 根据请求特定url => 将传入的参数datasource注入到模版引擎的window
@@ -12,15 +8,15 @@ const server = http.createServer((req, res) => {
 	// 解决跨域
 	res.setHeader("Access-Control-Allow-Origin","*");
 	res.setHeader("Access-Control-Allow-Headers","*");
-	// 解决相应数据中文乱码
-	res.setHeader("Content-Type","application/json;charset=utf-8");
 	// 路由模块统一处理，按mvc架构划分
 	if (req.url === '/demo.html') {
-		res.send(`
+		const dom = require('./static/entry/serverEntry')	// react服务端代码用es6需要在这里处理
+		const markup = dom.markup(req.url)
+		res.setHeader("Content-Type","text/html;charset=utf-8");
+		res.end(`
 			<!DOCTYPE html>
 				<head>
 					<title>React & React Router4 SSR</title>
-					<link rel="stylesheet" href="/css/main.css">
 				</head>
 				<body>
 				<div id="root">${markup}</div>
@@ -28,7 +24,9 @@ const server = http.createServer((req, res) => {
 			</html>
 		`);	
 	} else {
-		// routes(req, res)
+		// 解决相应数据中文乱码
+		res.setHeader("Content-Type","application/json;charset=utf-8");
+		routes(req, res)
 	}
 });
 server.listen(1234, () => {
--- server/static/pages/App.js
+++ server/static/pages/App.js
@@ -1,9 +1,20 @@
 import React from "react";
+import {hydrate} from 'react-dom'
 import {renderToString} from 'react-dom/server'
-import {StaticRouter} from 'react-router-dom'
+import {StaticRouter, BrowserRouter} from 'react-router-dom'
 
-export function markup (url) {
-    return renderToString(
-        <div>123</div>
-    )
+export function markup (url = '') {
+    if (url) {
+        return renderToString(
+            <StaticRouter context={{}} location={url}>
+                <div>server</div>
+            </StaticRouter>
+        )
+    } else {
+        return hydrate(
+            <BrowserRouter>
+                <div>client</div>
+            </BrowserRouter>
+        , document.getElementById('app'))
+    }
 }
